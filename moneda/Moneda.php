b489314dbdcfce629cf7fa8054bd8d65bf493af0
Jose Manuel Obando <obandogarcia10635@gmail.com>
2023-12-11 11:37:11 -0600

    Cambios finales
    Clase Tiempo completa y ajustes a la interfaz de ambos modulos

--- moneda/Moneda.php
+++ moneda/Moneda.php
@@ -22,6 +22,7 @@ class Yen implements IMoneda{
     public function convertir($moneda){
         //Valores de monedas respecto al dolar a $1 el 8/12/23
         $yen = 144.86;
+        $moneda = floatval($moneda);
         $resultado = $moneda * $yen;
         return "<h4>El valor ingresado en DOLARES equivalen a:<p class='text-success'>$resultado YENES</p><h4>";
     }
@@ -31,6 +32,7 @@ class FrancoSuizo implements IMoneda{
     public function convertir($moneda){
         //Valores de monedas respecto al dolar a $1 el 8/12/23
         $francoSuizo = 0.88;
+        $moneda = floatval($moneda);
         $resultado = $moneda * $francoSuizo;
         return "<h4>El valor ingresado en DOLARES equivalen a:<p class='text-success'>$resultado FRANCOS SUIZOS</p><h4>";
     }
@@ -40,6 +42,7 @@ class AUD implements IMoneda{
     public function convertir($moneda){
         //Valores de monedas respecto al dolar a $1 el 8/12/23
         $dolarAustraliano = 1.36;
+        $moneda = floatval($moneda);
         $resultado = $moneda * $dolarAustraliano;
         return "<h4>El valor ingresado en DOLARES equivalen a: <p class='text-success'>$resultado DOLARES AUSTRALIANOS</p><h4>";
     }
@@ -49,6 +52,7 @@ class LibraEsterlina implements IMoneda{
     public function convertir($moneda){
         //Valores de monedas respecto al dolar a $1 el 8/12/23
         $libraEsterlina= 0.80;
+        $moneda = floatval($moneda);
         $resultado = $moneda * $libraEsterlina;
         return "<h4>El valor ingresado en DOLARES equivalen a:<p class='text-success'>$resultado LIBRAS ESTERLINAS</p><h4>";
     }
--- tiempo/indexTiempo.php
+++ tiempo/indexTiempo.php
@@ -6,14 +6,86 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <title>Converitidor de tiempo</title>
+    <title>Convertidor de tiempo</title>
 </head>
 <body>
-    <header>
-        
+    <header class="container">
+        <h1>Conversor de Tiempo</h1>
     </header>
+    <section class="container">
+        <form action="" method="post" class="needs-validation" novalidate>
+            <div class="col-md-6">
+                <label for="convertir" class="form-label">Que deseas convertir?</label>
+                <select name="unidad" required>
+                    <option selected disabled value="">Seleccionar opcion...</option>
+                    <option value="segundo">Segundo</option>
+                    <option value="minuto">minuto</option>
+                    <option value="hora">hora</option>
+                    <option value="dia">dia</option>
+                    <option value="semana">semana</option>
+                    <option value="anio">anio</option>
+                </select>
+                <div class="valid-feedback">
+                    Seleccion correcta
+                </div>
+                <div class="invalid-feedback">
+                    Debes seleccionar una opcion
+                </div>
+            </div>
+            <div class="col-md-6">
+                <label for="valor" class="form-label">Cuanto quieres convertir?</label>
+                <input name="valor" type="number" step="0.01" class="form-control" min="0" required>
+                <div class="valid-feedback">
+                    Formato correcto
+                </div>
+                <div class="invalid-feedback">
+                    Este campo no puede estar vacio y debes ingresar solo numeros.
+                </div>
+            </div>
+            <br>
+            
+            <br>
+            <input class="btn btn-primary" type="submit" value="Convertir">
+        </form>
+    </section>
+    <section class="container">
+        <?php
+            require './Tiempo.php';
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+            if(isset($_POST['unidad'], $_POST['valor'])){
+                if($_POST['unidad'] == 'segundo'){
+                    $segundo = new Segundos();
+                    $segundo->calcular($_POST['valor']);
+                    echo $segundo->mostrarInformacion();
+                }elseif($_POST['unidad'] == 'minuto'){
+                    $minuto = new Minutos();
+                    $minuto->calcular($_POST['valor']);
+                    echo $minuto->mostrarInformacion();
+                }elseif($_POST['unidad'] == 'hora'){
+                    $hora = new Horas();
+                    $hora->calcular($_POST['valor']);
+                    echo $hora->mostrarInformacion();
+                }elseif($_POST['unidad'] == 'dia'){
+                    $dia = new Dias();
+                    $dia->calcular($_POST['valor']);
+                    echo $dia->mostrarInformacion();
+                }elseif($_POST['unidad'] == 'semana'){
+                    $semana = new Semanas();
+                    $semana->calcular($_POST['valor']);
+                    echo $semana->mostrarInformacion();
+                }elseif($_POST['unidad'] == 'anio'){
+                    $anio = new Anio();
+                    $anio->calcular($_POST['valor']);
+                    echo $anio->mostrarInformacion();
+                }else{
+                    echo "<h3>Valores ingresados no corresponden a valores validos de tiempo</h3>";
+                }
+            }
+        ?>
+    </section>
+
+    <script src="validaciones.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
 
